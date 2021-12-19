1b9fb899513224a8ab8c5b2cdce2ea643f0e08f1
tom.mok <tom.mok@nib.co.nz>
2021-12-20 12:59:21 +1300

    two by two det calculation

--- det.py
+++ det.py
@@ -12,4 +12,16 @@ def det(m: np.ndarray) -> float:
     :return: Determinant of the matrix
     """
 
+    if m.shape == (2, 2):
+        return (m[0][0] * m[1][1]) - (m[0][1] * m[1][0])
+
+    # iterate across top column
+    # for each element in top column multiply by inner matrix determinant
+    determinant  = 0
+
+    # for index, element in enumerate(m[0]):
+    #     innerMatrix = m[0:, 0:index index:-1]
+    #     determinant += element * det(innerMatrix)
+
+
     raise NotImplementedError()
